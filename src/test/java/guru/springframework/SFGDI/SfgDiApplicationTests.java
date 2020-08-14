65c1d32f06759e615eb56b6ca479fb79b4fbc347
Daniel Schragl <daniels@gmx.at>
2020-08-14 11:47:45 +0200

    Initial add.

---
+++ src/test/java/guru/springframework/SFGDI/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.SFGDI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
